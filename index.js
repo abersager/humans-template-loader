e57f273a110930e628b922fa392ee7fa5ef1627d
Oliver Spindler <os@oliverspindler.com>
2015-05-19 16:35:50 +0200

    Use humans and machines delimiters

--- index.js
+++ index.js
@@ -5,20 +5,25 @@ module.exports = function() {
 	var path = require('path');
 	var fs = require('fs');
 	var includeRegex = /<!--include\s+([\/\w\.]*?[\w]+\.[\w]+)-->/g;
-	
+	var templateSettings = {
+		evaluate    : /\{\{#([\s\S]+?)\}\}/g,
+		escape      : /\{\{([^#^=][\s\S]+?)\}\}/g,
+		interpolate : /\{\{=([\s\S]+?)\}\}/g,
+	};
+
 	var readFile = function(filepath, root) {
 		var self = readFile;
 		self.buffer = self.buffer || {};
-		
+
 		if (filepath in self.buffer)
 			return self.buffer[filepath];
-		
-		var content = readContent(fs.readFileSync(path.join(root, filepath), 'utf8'), root);    
+
+		var content = readContent(fs.readFileSync(path.join(root, filepath), 'utf8'), root);
 		self.buffer[filepath] = content;
 		return self.buffer[filepath];
 	};
-	
-	var readContent = function(content, root) {    
+
+	var readContent = function(content, root) {
 		var matches = includeRegex.exec(content);
 
 		while (matches != null) {
@@ -30,12 +35,12 @@ module.exports = function() {
 
 		return content;
 	};
-	
+
 	return function(content) {
 		this.cacheable && this.cacheable();
 		var callback = this.async();
 		content = readContent(content, this.context);
-		var fn = _.template(content);
+		var fn = _.template(content, templateSettings);
 		callback(null, "module.exports = " + fn.source + ";");
 	};
 }();
